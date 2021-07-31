443a677daf5461fda4bbac5eb40ea8055fb98313
Andrius <andriuss0316@gmail.com>
2021-07-31 18:00:47 +0300

    20210731

---
+++ main.js
@@ -0,0 +1,13 @@
+// septyniolikta a uzduotis
+
+// Kai kuriamas tik vienas select elementas
+
+const animals = [
+  'zuikis',
+  'barsukas',
+  'lape',
+  'vilkas',
+  'sernas',
+  'stirna',
+  'vovere',
+];
