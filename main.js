3113051c20122e29b9f577457d2a85c904957978
Andrius <andriuss0316@gmail.com>
2021-07-31 18:06:17 +0300

    20210731

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // septyniolikta b uzduotis
 
+const animals = [
+  'zuikis',
+  'barsukas',
+  'lape',
+  'vilkas',
+  'sernas',
+  'stirna',
+  'vovere',
+];
+
+const vegetables = ['morka', 'svogunas', 'bulve'];
+
+function renderSelect(selector, dataList) {
+  // your code goes here
+}
